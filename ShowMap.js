82e5f7c22959a2ff5511ab9ae16734d655528996
DyNamiC <imsong1997@gmail.com>
2020-04-02 00:23:39 +0900

    test

--- ShowMap.js
+++ ShowMap.js
@@ -1,12 +1,4 @@
-API_KEY = "2a7a6e03114918d4754b99f8d6b3894b"
-
 function getMap(){
-    fetch(
-        `//dapi.kakao.com/v2/maps/sdk.js?appkey=${API_KEY}`
-    ).then(function(response){
-        return response.json();
-    });
-    
     var container = document.getElementById('map');
     var options = {
         center: new kakao.maps.LatLng(33.450701, 126.570667),
