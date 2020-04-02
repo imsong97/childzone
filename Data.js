cb3fc2ac9ed8172cb4c3ce50ae54202f6e88d43c
DyNamiC <imsong1997@gmail.com>
2020-04-02 22:30:09 +0900

    Update Data.js

--- Data.js
+++ Data.js
@@ -28,5 +28,6 @@
 
 // getData();
 
-const myData = JSON.parse(data)
-console.log(myData[0].pageNo);
+var myxmlDoc= new ActiveXObject("Microsoft.XMLDOM");
+myxmlDoc.async = "false";
+console.log(myxmlDoc.load("저눅어린이보호구역표준데이터.xml"));
