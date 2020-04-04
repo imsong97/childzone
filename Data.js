ad45a92872a629e32a8490b25019559088dc6d3e
DyNamiC <imsong1997@gmail.com>
2020-04-04 21:19:56 +0900

    Update Data.js

--- Data.js
+++ Data.js
@@ -14,17 +14,17 @@
 //          console.log(data);
 //     }
 // });
-const API_KEY = "I%2Bu5yBZvAbz%2FgetqfPM1BQ%2FEShTpdA%2FCmaJOCUCSzvtSfbvPtETYt57IJ54u%2FmsQZRRQOLY12xGLJ1R4qHppJg%3D%3D";
+// const API_KEY = "I%2Bu5yBZvAbz%2FgetqfPM1BQ%2FEShTpdA%2FCmaJOCUCSzvtSfbvPtETYt57IJ54u%2FmsQZRRQOLY12xGLJ1R4qHppJg%3D%3D";
 
-function getData(){
-    fetch(
-        `http://api.data.go.kr/openapi/child-prtc-zn-std?ServiceKey=${API_KEY}`
-    ).then(function(response){
-        return response.json()
-    }).then(function(json){
-        console.log(json);
-    })
-}
+// function getData(){
+//     fetch(
+//         `http://api.data.go.kr/openapi/child-prtc-zn-std?ServiceKey=${API_KEY}`
+//     ).then(function(response){
+//         return response.json()
+//     }).then(function(json){
+//         console.log(json);
+//     })
+// }
 
-getData();
+// getData();
 
