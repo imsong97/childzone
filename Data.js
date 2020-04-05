5c21725d4a87faeaf608dfd591051a3f918d5e1c
DyNamiC <imsong1997@gmail.com>
2020-04-05 22:05:38 +0900

    0405

--- Data.js
+++ Data.js
@@ -27,7 +27,7 @@
 // getData();
 
 // 마커 클러스터러를 생성합니다 
-var clusterer = new kakao.maps.MarkerClusterer({
+const clusterer = new kakao.maps.MarkerClusterer({
     map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
     averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
     minLevel: 3 // 클러스터 할 최소 지도 레벨 
@@ -35,13 +35,13 @@ var clusterer = new kakao.maps.MarkerClusterer({
 
 // 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
 $.get("data.json", function(data) { // 데이터에서 좌표 값을 가지고 마커를 표시합니다
-    // var imageSrc = 'icon1.png', imageSize = new kakao.maps.Size(50, 50), // 마커이미지 크기
-    //     imageOption = {offset: new kakao.maps.Point(27, 69)}; 
-    // var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+    var imageSrc = 'icon1.png', imageSize = new kakao.maps.Size(50, 50), // 마커이미지 크기
+        imageOption = {offset: new kakao.maps.Point(27, 69)}; 
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 
     var markers = $(data).map( (data)=> {
         return new kakao.maps.Marker({
-            position : new kakao.maps.LatLng(data.latitude, data.longitude)
+            position : new kakao.maps.LatLng(latitude,longitude)
         });
     });
 
