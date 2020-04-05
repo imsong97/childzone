a4c2cb4e0468310fc4dbae8799ab44c023f4a7b4
DyNamiC <imsong1997@gmail.com>
2020-04-05 21:42:41 +0900

    Update ShowMap.js

--- ShowMap.js
+++ ShowMap.js
@@ -9,12 +9,10 @@ function getMap(currentLat, currentLon){
     };
     var map = new kakao.maps.Map(container, options);
 
-    var imageSrc = 'icon1.png', // 마커이미지의 주소입니다    
-    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
-    imageOption = {offset: new kakao.maps.Point(27, 69)}; 
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
     //마커 표시
+    var imageSrc = 'icon1.png', imageSize = new kakao.maps.Size(50, 50), // 마커이미지 크기
+        imageOption = {offset: new kakao.maps.Point(27, 69)}; 
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
     var markerPosition  = new kakao.maps.LatLng(currentLat, currentLon); 
     var marker = new kakao.maps.Marker({
         position: markerPosition,
