de7fff6599b8bd7ee8eafe4888a581a8768746ce
DyNamiC <imsong1997@gmail.com>
2020-04-05 21:33:18 +0900

    0405

--- ShowMap.js
+++ ShowMap.js
@@ -9,7 +9,7 @@ function getMap(currentLat, currentLon){
     };
     var map = new kakao.maps.Map(container, options);
 
-    var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다    
+    var imageSrc = 'icon.png', // 마커이미지의 주소입니다    
     imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
     imageOption = {offset: new kakao.maps.Point(27, 69)}; 
     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
