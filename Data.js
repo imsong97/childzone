3ebe297b7b2170f389de60150911f125a83c0105
DyNamiC <imsong1997@gmail.com>
2020-04-09 19:51:27 +0900

    Update Data.js

--- Data.js
+++ Data.js
@@ -9,7 +9,6 @@
 // }
 
 // getData();
-import showmap from "./ShowMap";
 
 var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div
     center : new kakao.maps.LatLng(), // 지도의 중심좌표 
