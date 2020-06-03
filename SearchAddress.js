a082642ac58499dfb77804f050c1f0c087d820a7
DyNamiC <imsong1997@gmail.com>
2020-06-03 15:52:25 +0900

    address search

---
+++ SearchAddress.js
@@ -0,0 +1,32 @@
+const search = document.querySelector(".searchButton");
+const address = document.querySelector(".searchInput");
+
+search.addEventListener("click", getAddress);
+
+function getAddress(){
+    var map = new kakao.maps.Map();
+
+    // 주소-좌표 변환 객체를 생성합니다
+    var geocoder = new kakao.maps.services.Geocoder();
+
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(address.value, function(result, status) {
+
+        // 정상적으로 검색이 완료됐으면 
+        if (status === kakao.maps.services.Status.OK) {
+
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            var marker = new kakao.maps.Marker({
+                map: map,
+                position: coords
+            });
+
+            marker.setMap(map);
+
+            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            map.setCenter(coords);
+        }
+    });    
+}
--- ShowMap.js
+++ ShowMap.js
@@ -1,6 +1,3 @@
-const search = document.querySelector(".searchButton");
-const address = document.querySelector(".searchInput");
-
 //현재위치 찾기
 navigator.geolocation.getCurrentPosition(function(position) { 
     const currentLat = position.coords.latitude, // 위도
@@ -28,63 +25,29 @@ function getMap(currentLat, currentLon){
     var zoomControl = new kakao.maps.ZoomControl();
     map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT); 
 
-    return map;
-}
-
-let map = getMap();
-console.log(map);
-
-// 마커 클러스터러를 생성합니다 
-const clusterer = new kakao.maps.MarkerClusterer({
-    map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
-    averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
-    minLevel: 5 // 클러스터 할 최소 지도 레벨 
-});
-
-// 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
-$.get("/data.json", function(data) { // 데이터에서 좌표 값을 가지고 마커를 표시합니다
-    var imageSrc = 'warning.png', imageSize = new kakao.maps.Size(50, 50), // 마커이미지 크기
-    imageOption = {offset: new kakao.maps.Point(25, 50)}; 
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
-    var markers = $(data.position).map(function(i, position) {
-        return new kakao.maps.Marker({
-            position : new kakao.maps.LatLng(position.latitude, position.longitude),
-            image: markerImage
-        });
+    // 마커 클러스터러를 생성합니다 
+    const clusterer = new kakao.maps.MarkerClusterer({
+        map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+        averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+        minLevel: 5 // 클러스터 할 최소 지도 레벨 
     });
 
-    // 클러스터러에 마커들을 추가합니다
-    clusterer.addMarkers(markers);
-});
+    // 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
+    $.get("/data.json", function(data) { // 데이터에서 좌표 값을 가지고 마커를 표시합니다
+        var imageSrc = 'warning.png', imageSize = new kakao.maps.Size(50, 50), // 마커이미지 크기
+        imageOption = {offset: new kakao.maps.Point(25, 50)}; 
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 
-search.addEventListener("click", getAddress);
-
-function getAddress(){
-
-    // 주소-좌표 변환 객체를 생성합니다
-    var geocoder = new kakao.maps.services.Geocoder();
-
-    // 주소로 좌표를 검색합니다
-    geocoder.addressSearch(address.value, function(result, status) {
-
-        // 정상적으로 검색이 완료됐으면 
-        if (status === kakao.maps.services.Status.OK) {
-
-            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-
-            // 결과값으로 받은 위치를 마커로 표시합니다
-            var marker = new kakao.maps.Marker({
-                map: map,
-                position: coords
+        var markers = $(data.position).map(function(i, position) {
+            return new kakao.maps.Marker({
+                position : new kakao.maps.LatLng(position.latitude, position.longitude),
+                image: markerImage
             });
+        });
 
-            marker.setMap(map);
-
-            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-            map.setCenter(coords);
-        } 
-    });    
+        // 클러스터러에 마커들을 추가합니다
+        clusterer.addMarkers(markers);
+    });
 }
 
 
